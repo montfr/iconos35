cb97b76168049d2399438f46decd70a193973ec4
Jonathan MirCha <jonmircha@gmail.com>
2017-10-20 19:08:50 -0500

    Operaciones CRUD: Update y Delete

--- BasesDatos/mexflix_crud.sql
+++ BasesDatos/mexflix_crud.sql
@@ -1,24 +1,31 @@
 /* Listado de operaciones CRUD */
 
 /* CREATE */
-INSERT INTO movie_series (imdb_id, title, plot, author, actors, genres, country, premiere, poster, trailer, rating, category) VALUES
-  ('tt0479143', 'Rocky Valvoa', '', 'Silvestre Stallone', ' Sylvester Stallone, Antonio Tarver, Milo Ventimiglia', 'Drama, Sport', 'Estados Unidos', 2006, '', '', 7.2, 'Movie');
+  INSERT INTO movie_series (imdb_id, title, plot, author, actors, genres, country, premiere, poster, trailer, rating, category) VALUES
+    ('tt0479143', 'Rocky Valvoa', '', 'Silvestre Stallone', ' Sylvester Stallone, Antonio Tarver, Milo Ventimiglia', 'Drama, Sport', 'Estados Unidos', 2006, '', '', 7.2, 'Movie');
 
-INSERT INTO movie_series (imdb_id, title, author, actors, genres, country, premiere, rating, category) VALUES
-  ('tt0479143', 'Rocky Valvoa', 'Silvestre Stallone', ' Sylvester Stallone, Antonio Tarver, Milo Ventimiglia', 'Drama, Sport', 'Estados Unidos', 2006, 7.2, 'Movie');
+  INSERT INTO movie_series (imdb_id, title, author, actors, genres, country, premiere, rating, category) VALUES
+    ('tt0479143', 'Rocky Valvoa', 'Silvestre Stallone', ' Sylvester Stallone, Antonio Tarver, Milo Ventimiglia', 'Drama, Sport', 'Estados Unidos', 2006, 7.2, 'Movie');
 
-INSERT INTO movie_series SET
-  title = 'Rocky Valvoa',
-  imdb_id = 'tt0479143',
-  actors = 'Sylvester Stallone, Antonio Tarver, Milo Ventimiglia',
-  author = 'Silvestre Stallone',
-  country = 'Estados Unidos',
-  premiere = 2006,
-  rating = 7.2,
-  genres = 'Drama, Sport',
-  category = 'Movie';
+  INSERT INTO movie_series SET
+    title = 'Rocky Valvoa',
+    imdb_id = 'tt0479143',
+    actors = 'Sylvester Stallone, Antonio Tarver, Milo Ventimiglia',
+    author = 'Silvestre Stallone',
+    country = 'Estados Unidos',
+    premiere = 2006,
+    rating = 7.2,
+    genres = 'Drama, Sport',
+    category = 'Movie';
+
 /* READ */
 
 /* UPDATE */
+  UPDATE movie_series SET
+    title = 'Rocky Balboa',
+    author = 'Sylvester Stallone',
+    poster = 'https://images-na.ssl-images-amazon.com/images/M/MV5BNWIyNmQyNjctYmVmMS00MGI4LWIxMmUtNjA0ODYzOTA0Yjk0L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY268_CR6,0,182,268_AL_.jpg'
+    WHERE imdb_id = 'tt0479143';
 
 /* DELETE */
+  DELETE FROM movie_series WHERE imdb_id = 'tt0479143';
