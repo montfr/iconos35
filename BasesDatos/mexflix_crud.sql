422576be1a0af1eef44f6168b66ab85a4f59b2ba
Jonathan MirCha <jonmircha@gmail.com>
2017-10-20 19:46:37 -0500

    Operaciones CRUD: READ

--- BasesDatos/mexflix_crud.sql
+++ BasesDatos/mexflix_crud.sql
@@ -19,6 +19,32 @@
     category = 'Movie';
 
 /* READ */
+  SELECT * FROM movie_series;
+
+  SELECT title, rating, author, premiere, country, genres, category FROM movie_series;
+
+  SELECT COUNT(*) FROM movie_series;
+
+  SELECT * FROM movie_series WHERE premiere = 2008;
+  SELECT * FROM movie_series WHERE country = 'Estados Unidos';
+
+  SELECT * FROM movie_series WHERE premiere IN (2008, 2009, 2010);
+
+  SELECT * FROM movie_series WHERE genres LIKE '%Drama';
+  SELECT * FROM movie_series WHERE genres LIKE 'Action%';
+  SELECT * FROM movie_series WHERE genres LIKE '%Drama%';
+
+  SELECT * FROM movie_series WHERE country LIKE '%Estados Unidos%' AND category = 'Serie';
+  SELECT * FROM movie_series WHERE country LIKE '%Estados Unidos%' AND NOT category = 'Movie';
+  SELECT * FROM movie_series WHERE country LIKE '%Estados Unidos%' AND category = 'Movie' OR category = 'Serie';
+
+  SELECT title, rating, author, premiere, country, genres, category FROM movie_series ORDER BY rating;
+
+  SELECT title, rating, author, premiere, country, genres, category FROM movie_series ORDER BY rating DESC;
+
+  SELECT title, rating, author, premiere, country, genres, category FROM movie_series ORDER BY premiere, rating;
+
+  SELECT title, rating, author, premiere, country, genres, category FROM movie_series ORDER BY premiere, rating DESC;
 
 /* UPDATE */
   UPDATE movie_series SET
