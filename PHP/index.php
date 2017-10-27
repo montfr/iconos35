4edd74e48ae5ef4c1da3c25159f55e8c844af34d
Jonathan MirCha <jonmircha@gmail.com>
2017-10-27 18:37:52 -0500

    Clase 5 BDs, subiendo teoría PHP

---
+++ PHP/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fundamentos de Programación en PHP</title>
+</head>
+<body>
+  <h1>Fundamentos de Programación en PHP</h1>
+  <p>Significa <b>Personal Home Pages</b> (PHP)</p>
+	<p>Creador Rasmus Lerdoff</p>
+	<p>Originalmente no se pensó como un lenguaje de programación.</p>
+	<p>Existen 3 formas de programar con PHP</p>
+  <ol>
+		<li>Como preprocesador de HTML</li>
+		<li>PHP Estructurado</li>
+		<li>PHP Orientado a Objetos</li>
+	</ol>
+  <h2>Fuentes:</h2>
+	<ul>
+		<li><a href="http://php.net/manual/es/" target="_blank">Documentación Oficial de PHP</a></li>
+		<li><a href="http://www.etnassoft.com/biblioteca/poo-y-mvc-en-php/" target="_blank">POO y MVC en PHP de Eugenia Bahit</a></li>
+		<li><a href="http://bextlan.com/tutoriales/php/" target="_blank">Tutoriales PHP</a></li>
+		<li><a href="http://bextlan.com/cursos/poo-php-mysql/" target="_blank">Curso de POO con PHP y MySQL</a></li>
+	</ul>
+  <h2>Ejercicios Básicos</h2>
+	<ul>
+		<li><a href="basicos.php">PHP Básico</a></li>
+		<li><a href="formularios.php">Formularios en PHP</a></li>
+	</ul>
+</body>
+</html>
