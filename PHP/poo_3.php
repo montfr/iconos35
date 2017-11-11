3e1e106f36cc8a3928fc000220b12c34bb1791ce
Jonathan MirCha <jonmircha@gmail.com>
2017-11-10 18:19:03 -0600

    Clase 2 PHP subiendo código inicial de la clase

---
+++ PHP/poo_3.php
@@ -0,0 +1,14 @@
+<?php
+
+echo '<h1>Abstracción en PHP</h1>';
+echo '<a href="./index.php">regresar</a>';
+
+echo '
+  <h2>Polígonos</h2>
+  <p>Figura geométrica plana que está limitada por tres o más rectas y tiene tres o más ángulos y vértices.</p>
+  <h3>Perímetro</h3>
+  <p>El perímetro de un polígono es igual a la suma de las longitudes de sus lados.</p>
+  <h3>Área</h3>
+  <p>El área de un polígono es la medida de la región o superficie encerrada por un polígono.</p>
+  <hr>
+';
